14ff1101be56d6a25434b1749a2052833addbf2e
Joshua Gray <jmg1138@gmail.com>
2017-01-12 15:30:14 -0800

    Working POST example.

--- app.js
+++ app.js
@@ -4,49 +4,17 @@
 
 // Web framework courtesy of https://github.com/expressjs/express
 var express = require('express');
-var app = express();
-var routes = require('./routes/routes.js')(app);
-var server = require('http').Server(app); // https://nodejs.org/api/http.html
-
-// Secure express app courtesy of https://github.com/helmetjs/helmet
-var helmet = require('helmet');
-
 // Parse requests courtesy of https://github.com/expressjs/body-parser
 var bodyParser = require('body-parser');
 
-// Free SSL courtesy of https://github.com/DylanPiercey/auto-sni
-// and https://letsencrypt.org/
-// var createServer = require("auto-sni");
+var port = process.env.PORT || 1138;
 
-// Just a few more things ...
-var port = 1138; // Used when process.env.PORT doesn't exist
-
-app.use(helmet());
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/*
-var sslSettings = {
-    email: "admin@grocereport.com", // Emailed when certificates expire.
-    agreeTos: true, // Required for letsencrypt.
-    debug: true, // Add console messages and uses staging LetsEncrypt server. (Disable in production)
-    forceSSL: true, // Make this false to disable auto http->https redirects (default true).
-    domains: [
-        ["grocereport.com"]
-    ],
-    redirectCode: 302, // If forceSSL is true, decide if redirect should be 301 (permanent) or 302 (temporary). Defaults to 302
-    ports: {
-        http: 3001, // Optionally override the default http port.
-        https: 3002 // // Optionally override the default https port.
-    }
-};
-var server = createServer(sslSettings, app);
-server.once("listening", () => {
-    console.log("Grocereport API server is running.");
-});
-*/
+var routes = require('./routes/routes.js')(app);
 
-
-server.listen(process.env.PORT || port, function() {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(port, function() {
+    console.log(`App running on http://localhost:${port}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -4,27 +4,8 @@ var appRouter = function(app) {
         res.send('hello');
     });
 
-    app.get('/login', function(req, res) {
-        var bundle = {
-            'username': 'nraboy',
-            'password': '1234',
-            'twitter': '@nraboy'
-        }
-        if (!req.query.username) {
-            return res.send({ 'status': 'error', 'message': 'missing username' });
-        } else if (req.query.username != accountMock.username) {
-            return res.send({ 'status': 'error', 'message': 'wrong username' });
-        } else {
-            return res.send(accountMock);
-        }
-    });
-
     app.post('/login', function(req, res) {
-        if (!req.body.username || !req.body.password || !req.body.twitter) {
-            return res.send({ 'status': 'error', 'message': 'missing a parameter' });
-        } else {
-            return res.send('Account created');
-        }
+        res.send(`Thank you ${req.body.name}`);
     });
 
 }
