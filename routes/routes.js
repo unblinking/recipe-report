cb25b03f922b6501cf8f97ff3106b95423b32405
Joshua Gray <jmg1138@gmail.com>
2017-01-18 13:06:35 -0800

    First unit tests using Mocha
    Regenerated JSDoc pages. Switched JSDoc pages to use the Darkly theme.
    Wrote the first three unit tests using Mocha.

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * Routes for the Grocereport API.
- * Define application end points and how they respond to client requests.
+ * Define application end points (routes) for the Grocereport API and how they respond to client requests.
  * @namespace routes
  * @public
  * @author jmg1138 {@link https://github.com/jmg1138 jmg1138 on GitHub}
@@ -16,36 +15,62 @@
 'use strict';
 
 /**
- * @namespace appRouter
- * @memberof routes
  * @public
+ * @namespace router
+ * @memberof routes
  * @param {object} app - The Express application instance.
  * @see {@link https://expressjs.com/en/guide/routing.html Express routing}
- * @see {@link http://expressjs.com/en/api.html#app Express API app}
+ * @see {@link http://expressjs.com/en/api.html Express API}
  */
-var appRouter = function(app) {
+var router = function(app) {
 
     /**
-     * @function app.get
-     * @memberof! routes.appRouter
+     * GET request to the root route. Responds with the JSON object { message: "hello" }
      * @public
-     * @summary GET request to the root route. Sends a response of 'hello' when a GET request is made to the root route.
+     * @function .app.get('/')
+     * @memberof! routes.router
      * @param {object} req - The HTTP request.
      * @param {object} res - The HTTP response.
      * @example
-     * www.grocereport.com/
-     * @see {@link https://expressjs.com/en/api.html#app.get Express API app.get}
-     * @see {@link http://expressjs.com/en/api.html#req Express API req}
-     * @see {@link http://expressjs.com/en/api.html#res Express API res}
-     * @see {@link http://expressjs.com/en/api.html#res.send Express API res.send}
+     * // Responds with the JSON object { message: "hello" }
+     * var request = require('request');
+     * request('http://www.grocereport.com/',
+     *     function(err, res, body) {
+     *         if (!err && res.statusCode == 200) {
+     *             console.log(body);
+     *         }
+     *     });
+     * @see {@link https://expressjs.com/en/api.html Express API}
      */
     app.get('/', function(req, res) {
-        res.send('hello');
+        res.status(200).json({ message: 'hello' });
+    });
+
+    /**
+     * GET request to the /test route. Responds with the JSON object { message: "Welcome to the team, DZ-015" }
+     * @public
+     * @function app.get('/test)
+     * @memberof! routes.router
+     * @param {object} req - The HTTP request.
+     * @param {object} res - The HTTP response.
+     * @example
+     * // Responds with the JSON object { message: "Welcome to the team, DZ-015" }
+     * var request = require('request');
+     * request('http://www.grocereport.com/test',
+     *     function(err, res, body) {
+     *         if (!err && res.statusCode == 200) {
+     *             console.log(body);
+     *         }
+     *     });
+     * @see {@link https://expressjs.com/en/api.html Express API}
+     */
+    app.get('/test', function(req, res) {
+        res.status(200).json({ message: 'Welcome to the team, DZ-015' });
     });
 
     /**
      * @function app.post
-     * @memberof! routes.appRouter
+     * @memberof! routes.router
      * @public
      * @summary POST request to the login route. Sends a response of 'Thank you' when a POST request is made to the login route.
      * @param {object} req - The HTTP request.
@@ -61,14 +86,10 @@ var appRouter = function(app) {
      * request.post(options, function(err, res, body) {
      *     console.log(body);
      * });
-     * @see {@link https://expressjs.com/en/api.html#app.post Express API app.post}
-     * @see {@link http://expressjs.com/en/api.html#req Express API req}
-     * @see {@link http://expressjs.com/en/api.html#res Express API res}
-     * @see {@link http://expressjs.com/en/api.html#res.send Express API res.send}
-     * @see {@link http://expressjs.com/en/api.html#req.body Express API req.body}
+     * @see {@link https://expressjs.com/en/api.html Express API}
      */
     app.post('/login', function(req, res) {
-        res.send(`Thank you ${req.body.name}`);
+        res.status(200).json({ name: req.body.name });
     });
 
 };
@@ -78,4 +99,4 @@ var appRouter = function(app) {
  * @see {@link https://nodejs.org/api/modules.html#modules_the_module_object Nodejs modules: The module object}
  * @see {@link https://nodejs.org/api/modules.html#modules_module_exports Nodejs modules: module exports}
  */
-module.exports = appRouter;
+module.exports = router;
--- test/post.login.test.js
+++ test/post.login.test.js
@@ -1,1 +1,25 @@
+// Set the NODE_ENV environmental variable to test
 process.env.NODE_ENV = 'test';
+
+var app = require('../app');
+var request = require('supertest');
+var should = require('should');
+
+describe('POST /login', function() {
+    it('should respond with the JSON object { name: username } where username is the name being sent with the POST request', function(done) {
+        username = new Date().getTime();
+        request(app)
+            .post('/login')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .send({ name: username })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect({ name: username })
+            .end(function(err, res) {
+                if (err) return done(err);
+                console.log(`RESPONSE BODY: ${JSON.stringify(res.body, null, 4)}`);
+                done();
+            });
+    });
+});
