da689dfef30fae48b7774932187851d3fc31ac13
Joshua <jmg1138@gmail.com>
2017-06-28 14:20:43 -0700

    More rewrites

--- test/jwt/generateToken.js
+++ test/jwt/generateToken.js
@@ -15,6 +15,9 @@ const accountModel = require("../../models/account");
 const jwt = require("../../util/jwt");
 const theMoment = require("moment");
 
+/**
+ * Account model to use during the tests.
+ */
 const account = new accountModel({
   email: process.env.MOCHA_USERNAME
 })
@@ -22,37 +25,36 @@ const account = new accountModel({
 /**
  * Tests.
  */
-describe("JSON Web Token generator", () =>
-  it(`should generate and return a JSON web token that can be decoded`, () =>
+describe(`JSON Web Token tests.`, () =>
+  it(`Should generate and return a JSON web token that can be decoded`, () =>
     jwt.generateToken(account)
     .then(token => jwt.decodeToken(token))
     .then(decoded => {
       describe("JWT headers", () => {
-        it(`Header algorithm should equal "HS256".`, () =>
+        it(`Algorithm should equal "HS256".`, () =>
           decoded.header.alg.should.equal("HS256") // Different algorithm than production
         );
-        it(`Header type should equal "JWT".`, () =>
+        it(`Type should equal "JWT".`, () =>
           decoded.header.typ.should.equal("JWT")
         );
       });
-      describe("JWT Payload", () => {
-        it(`Payload data should equal the account model ID.`, () =>
+      describe("JWT payload", () => {
+        it(`Data should equal the account model ID.`, () =>
           decoded.payload.data.toString().should.equal(account._id.toString())
         );
-        it(`Payload issued date (in seconds) equals today.`, () => {
+        it(`Issued date (in seconds) should equal today.`, () => {
           (typeof decoded.payload.iat).should.equal("number");
           const itWasIssued = decoded.payload.iat * 1000;
           theMoment(itWasIssued).isSame(Date.now(), "day").should.equal(true);
         });
-        it(`Payload expiration date is in 2 days.`, () => {
+        it(`Expiration date is in 2 days.`, () => {
           (typeof decoded.payload.exp).should.equal("number");
           const itExpires = decoded.payload.exp * 1000;
           theMoment(itExpires).fromNow().should.equal("in 2 days");
         });
       });
-
       //console.log(decoded);
-
     })
   )
+
 );
