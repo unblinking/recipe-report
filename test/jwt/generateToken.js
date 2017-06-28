e11bb1ae2a56f8b5318ac8022de582177c37db53
Joshua <jmg1138@gmail.com>
2017-06-28 12:47:22 -0700

    Rewritten test for generateToken.js

--- test/jwt/generateToken.js
+++ test/jwt/generateToken.js
@@ -13,6 +13,7 @@
  */
 const accountModel = require("../../models/account");
 const jwt = require("../../util/jwt");
+const theMoment = require("moment");
 
 const account = new accountModel({
   email: process.env.MOCHA_USERNAME
@@ -21,18 +22,37 @@ const account = new accountModel({
 /**
  * Tests.
  */
-describe("JSON web token generation", () =>
-  it(`should generate and return a JSON web token that can be decoded, with
-      header.alg of "HS256", header.typ of "JWT", payload.data of the account
-      model ID, payload.iat of a number, and payload.exp of a number.`, () =>
+describe("JSON Web Token generator", () =>
+  it(`should generate and return a JSON web token that can be decoded`, () =>
     jwt.generateToken(account)
     .then(token => jwt.decodeToken(token))
     .then(decoded => {
-      decoded.header.alg.should.equal("HS256"); // Different algorithm than production
-      decoded.header.typ.should.equal("JWT");
-      JSON.stringify(decoded.payload.data).should.equal(JSON.stringify(account._id));
-      (typeof decoded.payload.iat).should.equal("number");
-      (typeof decoded.payload.exp).should.equal("number");
+      describe("JWT headers", () => {
+        it(`Header algorithm should equal "HS256".`, () =>
+          decoded.header.alg.should.equal("HS256") // Different algorithm than production
+        );
+        it(`Header type should equal "JWT".`, () =>
+          decoded.header.typ.should.equal("JWT")
+        );
+      });
+      describe("JWT Payload", () => {
+        it(`Payload data should equal the account model ID.`, () =>
+          decoded.payload.data.toString().should.equal(account._id.toString())
+        );
+        it(`Payload issued date (in seconds) equals today.`, () => {
+          (typeof decoded.payload.iat).should.equal("number");
+          const itWasIssued = decoded.payload.iat * 1000;
+          theMoment(itWasIssued).isSame(Date.now(), "day").should.equal(true);
+        });
+        it(`Payload expiration date is in 2 days.`, () => {
+          (typeof decoded.payload.exp).should.equal("number");
+          const itExpires = decoded.payload.exp * 1000;
+          theMoment(itExpires).fromNow().should.equal("in 2 days");
+        });
+      });
+
+      //console.log(decoded);
+
     })
   )
 );
