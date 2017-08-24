0ec4f792239125e8853104d875fa4affc9d6dd1b
Joshua <jmg1138@gmail.com>
2017-08-24 13:01:57 -0700

    Reorganized app.js into promises used by async/await

--- app.js
+++ app.js
@@ -12,25 +12,30 @@
  * @see {@link https://github.com/expressjs/body-parser body-parser}
  * @see {@link https://github.com/expressjs/express expressjs}
  * @see {@link https://github.com/helmetjs helmetjs}
- * @see {@link https://github.com/nodenica/node-heroku-ssl-redirect ssl-redirect}
+ * @see {@link https://github.com/nodenica/node-heroku-ssl-redirect heroku ssl}
+ * @see {@link https://nodejs.org/api/http.html http}
  * @see {@link https://github.com/Automattic/mongoose mongoose}
  * @see {@link https://github.com/jaredhanson/passport passport}
  *
  */
 const bodyParser = require('body-parser')
-const express = require('express')
+const expressjs = require('express')
 const helmet = require('helmet')
 const herokuSslRedirect = require('heroku-ssl-redirect')
+const http = require('http')
 const mongoose = require('mongoose')
 const packageJson = require('./package.json')
 const passport = require('passport')
-const respond = require('./lib/respond')
 const routes = require('./routes/routes')
 
 /**
  * Something fun!
+ * @see {@link http://patorjk.com/software/taag/#p=display&h=2&v=2&f=Standard&t=Recipe%0AReport TAAG}
+ * @see {@link https://stackoverflow.com/a/41407246 nodejs console font color}
  */
-console.log(`\x1b[1m\x1b[32m
+function somethingFun () {
+  return new Promise(resolve => {
+    console.log(`\x1b[1m\x1b[32m
   ____           _
  |  _ \\ ___  ___(_)_ __   ___
  | |_) / _ \\/ __| | '_ \\ / _ \\
@@ -41,7 +46,10 @@ console.log(`\x1b[1m\x1b[32m
  |  _ <  __/ |_) | (_) | |  | |_
  |_| \\_\\___| .__/ \\___/|_|   \\__|
            |_|     \x1b[37m version ${packageJson.version}
-\x1b[0m`)
+    \x1b[0m`)
+    resolve()
+  })
+}
 
 /**
  * Connect mongoose to the MongoDB datastore.
@@ -56,45 +64,101 @@ async function dbConnect () {
     ${JSON.stringify(err)}`)
   }
 }
-dbConnect()
 
 /**
- * Instantiate the Express application and define all app configurations here except routes (define routes last).
+ * Instantiate the expressjs application.
  */
-const app = express()
-app.use(helmet())
-if (process.env.NODE_ENV === 'production') app.use(herokuSslRedirect())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true // When true uses {@link https://github.com/ljharb/qs qs} querystring parsing.
-}))
-app.use(passport.initialize())
-app.set('json spaces', 2)
+function expressInstance () {
+  return new Promise(resolve => {
+    let express = expressjs()
+    resolve(express)
+  })
+}
 
 /**
- * Define routes last, after all other configurations.
- * @param {object} app - The Express application instance.
+ * Configure the expressjs application.
+ * Define all expressjs configurations here (except routes, define routes last).
+ * @param {Object} express The expressjs instance.
  */
-routes(app)
+function expressConfigure (express) {
+  return new Promise(resolve => {
+    express.use(helmet())
+    if (process.env.NODE_ENV === 'production') express.use(herokuSslRedirect())
+    express.use(bodyParser.json())
+    express.use(bodyParser.urlencoded({
+      extended: true // When true uses {@link https://github.com/ljharb/qs qs} querystring parsing.
+    }))
+    express.use(passport.initialize())
+    express.set('json spaces', 2)
+    resolve()
+  })
+}
 
 /**
- * Listen for connections on the specified port.
- * @see {@link https://expressjs.com/en/api.html#app.listen expressjs listen}
- * @see {@link http://patorjk.com/software/taag/#p=display&h=2&v=2&f=Standard&t=Recipe%0AReport TAAG}
- * @see {@link https://stackoverflow.com/a/41407246 nodejs console font color}
+ * Define the expressjs routes.
+ * @param {Object} express The expressjs instance.
+ * @see {@link https://expressjs.com/en/guide/routing.html expressjs routing}
  */
-const port = parseInt(process.env.PORT, 10) || 1138
-app.listen(port, () =>
-  console.log(` \x1b[1m\x1b[33m>\x1b[0m \x1b[1m\x1b[36mListening on port ${port}\x1b[0m`)
-).on('error', err => console.log(err))
-// TODO: If error, try again a number of times and then give up.
+function expressRoutes (express) {
+  return new Promise(resolve => {
+    routes(express)
+    resolve()
+  })
+}
 
 /**
- * Define error-handling middleware after app and route configurations.
+ * Define the expressjs error handling middleware.
+ * @param {Object} express The expressjs instance.
  */
-app.use((req, res, next) => {
-  respond.error(res, new Error('four, oh four!'))
-})
-app.use((err, req, res, next) => respond.error(res, err))
+function expressErrors (express) {
+  return new Promise(resolve => {
+    express.use((req, res, next) => res.status(404).send('four, oh four!'))
+    express.use((err, req, res, next) => {
+      res.status(500).send('Something broke!')
+      console.log(err.message)
+    })
+    resolve()
+  })
+}
 
-module.exports = app // For testing with supertest
+/**
+ * Instantiate the http server.
+ * @param {Object} express The expressjs instance.
+ */
+function serverInstance (express) {
+  return new Promise(resolve => {
+    let server = http.Server(express)
+    resolve(server)
+  })
+}
+
+/**
+ * Listen for http server connections.
+ * @param {Object} server The http server instance.
+ */
+function serverListen (server) {
+  return new Promise(resolve => {
+    const port = parseInt(process.env.PORT, 10) || 1138
+    server.listen(port, () => {
+      console.log(` \x1b[1m\x1b[33m>\x1b[0m \x1b[1m\x1b[36mListening on port ${port}\x1b[0m`)
+      resolve()
+    })
+  })
+}
+
+/**
+ * Create the API parts in proper order.
+ */
+async function main () {
+  await somethingFun()
+  await dbConnect()
+  let express = await expressInstance()
+  await expressConfigure(express)
+  await expressRoutes(express)
+  await expressErrors(express)
+  let server = await serverInstance(express)
+  await serverListen(server)
+}
+exports.main = main // For testing with supertest
+
+main()
