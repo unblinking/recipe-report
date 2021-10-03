6be389d1dd8c7cf0b6eee901fbbd0bd02a87644c
jmg1138 <joshua@unblinking.io>
2021-10-03 06:03:20 -0700

    Change the license from MIT to GNU AGPLv3 going forward.

--- dist/factories/fun-factory.js
+++ dist/factories/fun-factory.js
@@ -3,6 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.graffiti = void 0;
 const log_1 = require("../wrappers/log");
 const version = process.env.npm_package_version;
+const stage = `Alpha`;
+const mode = process.env.NODE_ENV || `development`;
+const license = process.env.npm_package_license;
+const repository = `https://github.com/nothingworksright/api.recipe.report`;
 const piece = `\x1b[1m\x1b[32m
   ____           _
  |  _ \\ ___  ___(_)_ __   ___
@@ -13,7 +17,12 @@ const piece = `\x1b[1m\x1b[32m
  | |_) / _ \\ '_ \\ / _ \\| '__| __|
  |  _ <  __/ |_) | (_) | |  | |_
  |_| \\_\\___| .__/ \\___/|_|   \\__|
- \x1b[37mAlpha     \x1b[1m\x1b[32m|_|      \x1b[37mversion ${version}
+           |_|      \x1b[37mversion ${version}
+ 
+ Release is ${stage}
+ Running in ${mode} mode
+ License ${license}
+ Repository ${repository}
  \x1b[0m`;
 exports.graffiti = log_1.logger.wrap(function graffiti() {
     log_1.logger.info(piece);
--- src/factories/fun-factory.ts
+++ src/factories/fun-factory.ts
@@ -7,6 +7,11 @@
 import { logger } from '../wrappers/log'
 
 const version: string = process.env.npm_package_version as string
+const stage: string = `Alpha`
+const mode: string = (process.env.NODE_ENV as string) || `development`
+const license: string = process.env.npm_package_license as string
+const repository: string = `https://github.com/nothingworksright/api.recipe.report`
+
 const piece: string = `\x1b[1m\x1b[32m
   ____           _
  |  _ \\ ___  ___(_)_ __   ___
@@ -17,7 +22,12 @@ const piece: string = `\x1b[1m\x1b[32m
  | |_) / _ \\ '_ \\ / _ \\| '__| __|
  |  _ <  __/ |_) | (_) | |  | |_
  |_| \\_\\___| .__/ \\___/|_|   \\__|
- \x1b[37mAlpha     \x1b[1m\x1b[32m|_|      \x1b[37mversion ${version}
+           |_|      \x1b[37mversion ${version}
+ 
+ Release is ${stage}
+ Running in ${mode} mode
+ License ${license}
+ Repository ${repository}
  \x1b[0m`
 
 export const graffiti = logger.wrap(function graffiti(): void {
