56f52af92ec79b250065fecccceebaa4c75b8b25
jmg1138 <joshua@unblinking.io>
2021-11-05 23:43:48 -0700

    Modified the service response abstract class to include `fail` and `statusCode` arguments. Because the overall API responds with one of three main possibilities, success/fail/error, I want the service response to be able to specify one of those three as well. If a failure is encountered, due to the client that is using the API, fail will be marked as true, and an error will be provided. If an unexpected server error occurs, such as in a catch, then both success and fail will be false, but an error will be present. A status code can be set by the service, to indicate specific failure codes. These are HTTP status codes.

--- src/db/models/service-responses.ts
+++ src/db/models/service-responses.ts
@@ -26,19 +26,32 @@
 
 interface IServiceResponse<T> {
   success?: boolean
+  fail?: boolean
   error?: Error
   item?: T
+  statusCode?: number
 }
 
 abstract class ServiceResponse<T> implements IServiceResponse<T> {
   private state: IServiceResponse<T> = {}
 
-  constructor(success: boolean = false, error?: Error, item?: T) {
+  constructor(
+    success: boolean = false,
+    fail: boolean = false,
+    error?: Error,
+    item?: T,
+    statusCode?: number
+  ) {
     this.setSuccess(success)
+    this.setFail(fail)
     this.setError(error)
     this.setItem(item)
+    this.setStatusCode(statusCode)
   }
 
+  // Success or not?
+  // Did the service do what it wanted to do successfully?
+  // This would normally return a status code 200.
   public get success(): boolean | undefined {
     return this.state.success
   }
@@ -46,6 +59,19 @@ abstract class ServiceResponse<T> implements IServiceResponse<T> {
     this.state.success = success
   }
 
+  // Fail or not?
+  // Did the service have some problem caused by the client?
+  // This would normally return a status code 400.
+  public get fail(): boolean | undefined {
+    return this.state.fail
+  }
+  public setFail(fail: boolean | undefined): void {
+    this.state.fail = fail
+  }
+
+  // Error object.
+  // An error happened during the request.
+  // This would normally return a status code 500.
   public get error(): Error | undefined {
     return this.state.error
   }
@@ -53,11 +79,26 @@ abstract class ServiceResponse<T> implements IServiceResponse<T> {
     this.state.error = error
   }
 
+  // Item requested.
+  // If some item was requested, respond with the item here.
+  // Examples: a user object, or an authentication token.
   public get item(): T | undefined {
     return this.state.item
   }
   public setItem(item: T | undefined): void {
     this.state.item = item
+  }
+
+  // HTTP status code.
+  // @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status}
+  // Depending on success (200), fail(400), or error (500), this should return a
+  // proper status code. Could be any standard HTTP status code, but it would be
+  // nice if we could limit to the three main status codes for simplicity.
+  public get statusCode(): number | undefined {
+    return this.state.statusCode
+  }
+  public setStatusCode(statusCode: number | undefined): void {
+    this.state.statusCode = statusCode
   }
 }
 
