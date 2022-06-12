fb1622fe23bc3034a79c1f47cc0fd262e27515d5
jmg1138 <joshua@unblinking.io>
2022-06-11 20:52:44 -0700

    These are the files that got "moved" from the web package to the react-app package, missed in the previous commit.

---
+++ packages/react-app/src/components/alert/Alert.tsx
@@ -0,0 +1,63 @@
+/**
+ * Alert div component.
+ *
+ * @author Joshua Gray {@link https://github.com/jmg1138}
+ * @copyright Copyright (C) 2017-2022
+ * @license GNU AGPLv3 or later
+ *
+ * This file is part of Recipe.Report.
+ * @see {@link https://github.com/unblinking/recipe-report}
+ *
+ * Recipe.Report is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Recipe.Report is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @module
+ */
+import styles from '../../components/alert/Alert.module.css'
+import icons from '../../components/alert/AlertIcon.module.css'
+
+export const alertStyles = {
+  PRIMARY: `primary`,
+  SECONDARY: `secondary`,
+  SUCCESS: `success`,
+  ERROR: `error`,
+  WARNING: `warning`,
+  INFO: `info`,
+  LIGHT: `light`,
+  DARK: `dark`,
+  SPIN: `spin`,
+} as const
+type alertStylesType = typeof alertStyles
+export type alertStylesKeyType = keyof alertStylesType
+export type alertStylesValueType = alertStylesType[keyof alertStylesType]
+
+interface AlertProps {
+  style: alertStylesValueType
+  title?: string
+  message?: string | undefined
+  code?: string | undefined
+}
+
+export function Alert({ style, title, message, code }: AlertProps): JSX.Element {
+  return (
+    <div className={styles[style]}>
+      {title && (
+        <span className={styles['title']}>
+          <span className={icons[style]}></span> {title}
+        </span>
+      )}
+      {message && <span className={styles['message']}>{message}</span>}
+      {code && <span className={styles['code']}>Code: {code}</span>}
+    </div>
+  )
+}
---
+++ packages/react-app/src/wrappers/jwt.ts
@@ -0,0 +1,40 @@
+/**
+ * JWT.
+ *
+ * Typescript helper functions for working with the JSON Web Token (JWT).
+ * These are specific to the recipe.report JWT.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API}
+ *
+ * Based on a blog post by Alexander Eckert, July 12, 2020.
+ * @see {@link https://eckertalex.dev/blog/typescript-fetch-wrapper}
+ *
+ * @author Joshua Gray {@link https://github.com/jmg1138}
+ * @copyright Copyright (C) 2017-2022
+ * @license GNU AGPLv3 or later
+ *
+ * This file is part of Recipe.Report.
+ * @see {@link https://github.com/unblinking/recipe-report}
+ *
+ * Recipe.Report is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Recipe.Report is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @module
+ */
+import type { Claims } from '@recipe-report/service/jwt-service'
+import jwt_decode from 'jwt-decode'
+
+export function parseJwt(token: string): Claims {
+  const payload: Claims = jwt_decode(token)
+  return payload
+}
